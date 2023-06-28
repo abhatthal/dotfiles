a29a4c5988ead3543afb6c79d5e46b8703d759c5
Kash Bhatthal <kashb@google.com>
2023-06-28 00:20:59 -0700

    Updated screenlayouts: Changed bindings, removed left layout, added dual layout

---
+++ .screenlayout/dual.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 1857x1765 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --mode 2560x1440 --pos 1440x325 --rotate normal --output HDMI-2 --off --output DP-1-8 --mode 2560x1440 --pos 0x0 --rotate right --output DP-1-1 --off
+$HOME/.screenlayout/set_wallpaper.sh
